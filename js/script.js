d22123fc61b3388cf8823d98808b28d0c504405e
stephanycasillas10 <s_stephany.casillas@ousd.org>
2019-10-16 01:30:34 +0000

    I made the button work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $( "document" ).ready(function() {
 
+    $("#button").click(function(){
+        let output=$(".input").val();
+        let checkVowelResult=checkVowel(output)
+        $(".output").text(checkVowelResult);
 
+});
+function checkVowel(str){
+  var firstLetter= str [0];
+  var vowels=['a','e','i','o','u'];
 
+  if (vowels.includes (firstLetter)){
+    return 'starts with vowel';
+  }
+
+  return 'starts with consonant';
+}
 });

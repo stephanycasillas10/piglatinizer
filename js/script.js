0b892e326add94a6573b587058ae3f1471b3f2ca
stephanycasillas10 <s_stephany.casillas@ousd.org>
2019-10-23 01:04:50 +0000

    I finished the project by adding a pigLatinizer function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
 $( "document" ).ready(function() {
 
     $("#button").click(function(){
-        let output=$(".input").val();
-        let checkVowelResult=checkVowel(output)
-        $(".output").text(checkVowelResult);
-
+        let userInput=$(".input").val();
+        let checkVowelResult=checkVowel(userInput)
+        let pigLatinWord=pigLatinizer(userInput);
+        $(".output").text(pigLatinWord);
 });
+
+function pigLatinizer(userInput){
+    let isVowel= checkVowel(userInput)
+
+    if (isVowel===false){
+        return user.slice(1)+ userInput[0]+"ay"
+    }
+        return userInput +"ay"
+}
+
+function sliceFromSecond(str){
+  return str.slice(1)
+}
+
 function checkVowel(str){
   var firstLetter= str [0];
   var vowels=['a','e','i','o','u'];
 
   if (vowels.includes (firstLetter)){
-    return 'starts with vowel';
+    return true;
   }
 
-  return 'starts with consonant';
+  return false;
 }
 });
